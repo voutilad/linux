e7407952e9d0973a22b59ae0aa122c6b2954792c
Dave Voutila <voutilad@gmail.com>
2020-05-11 17:39:13 -0400

    First cut at detecting OpenBSD hypervisor
    Some quick and dirty tweaks. Can confirm detection at boot, but had a
    kernel panic after mounting the root disk. Not sure if it's related, but
    this is a good first step.

---
+++ arch/x86/kernel/cpu/openbsd_vmm.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * OpenBSD VMM detection code.
+ *
+ * Copyright (C) 2020
+ * Author: Dave Voutila <voutilad@gmail.com>
+ */
+
+#include <linux/types.h>
+#include <linux/printk.h>
+#include <asm/hypervisor.h>
+
+#if IS_ENABLED(CONFIG_OPENBSD_VMM_GUEST)
+
+static uint32_t __init vmm_detect(void)
+{
+	return hypervisor_cpuid_base("OpenBSDVMM58", 0);
+}
+
+static void __init vmm_init_platform(void)
+{
+	// todo
+	pr_info("[%s] XXX OPENBSD VMM detected?", __func__);
+}
+
+const __initconst struct hypervisor_x86 x86_hyper_openbsd_vmm = {
+	.name			= "OpenBSD VMM",
+	.detect			= vmm_detect,
+	.type			= X86_HYPER_OPENBSD_VMM,
+	.init.init_platform	= vmm_init_platform,
+};
+#endif
